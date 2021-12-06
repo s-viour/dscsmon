4b64b28a3a278118f793ff32239435da7fe642fd
Matt Wyatt <mwyatt1000@gmail.com>
2021-12-05 20:41:47 -0500

    akhjsdfjksafasf

--- dscs3a3mon/monitor.py
+++ dscs3a3mon/monitor.py
@@ -14,6 +14,9 @@ class Monitor:
     def command(self, cmd):
         params = {}
         params['command'] = cmd
+        self.session.post(self.webhook, {
+            'content': f'checking command {cmd}'
+        })
         r = self.session.get(self.baseurl, params=params)
         if self.cache.get(cmd) is None:
             logging.info(f'populated cache for command {cmd}')
